e8467eb62e37ae0077539b9ba92920309e22a26e
shridharkapshikar <shridhar.kapshikar@hotmail.com>
2019-08-23 10:01:32 +0530

    init script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Install Jenkins stable release"
+yum remove -y java
+yum install -y java-1.8.0-openjdk
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+chkconfig jenkins on
+
+
+echo "Install Ansible stable release"
+
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+yum repolist
+yum — enablerepo=epel install ansible
+useradd -d /home/ansadm
