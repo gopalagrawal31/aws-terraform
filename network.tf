2e8991521a1e4644ff525c76cef85b9f878bdf0b
shridharkapshikar <shridhar.kapshikar@hotmail.com>
2019-08-23 16:58:22 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -6,7 +6,7 @@ tags = {
 }
 
 resource "aws_route_table" "dev-public-crt" {
-    vpc_id = "${aws_vpc.main-vpc.id}"
+    vpc_id = "${aws_vpc.dev-vpc.id}"
     
     route {
         cidr_block = "0.0.0.0/0"         
