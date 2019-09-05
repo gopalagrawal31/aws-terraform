180614db849886eb9d7fd348e30659a8a16abe72
shridharkapshikar <shridhar.kapshikar@hotmail.com>
2019-09-05 22:29:25 +0530

    Update setup.sh

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,1 +1,20 @@
-!#/bin/bash
+#!/bin/bash
+echo "Install Jenkins stable release"
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+chkconfig jenkins on
+
+echo "Install maven & git"
+sudo apt-get install  maven git-core
+
+echo "Install Ansible stable release"
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
