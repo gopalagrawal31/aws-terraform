cbe430f982726ceed25526e276ceb289d3fb33c3
shridharkapshikar <shridhar.kapshikar@hotmail.com>
2019-09-12 12:15:52 +0530

    Update tomcat_setup.sh

--- tomcat_setup.sh
+++ tomcat_setup.sh
@@ -1,10 +1,26 @@
-echo "Installing jav"
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+#!/bin/bash
 
-echo "Installing tomcat, maven & git-core"
-sudo apt-get update
-sudo apt-get install tomcat7-docs tomcat7-admin tomcat7-examples
-sudo service tomcat7 restart
+# add dedicated user 
+sudo groupadd tomcat
+sudo mkdir /opt/tomcat
+sudo useradd -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
 
+#download and install apache tomcat
+cd ~
+wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+sudo tar -zxvf apache-tomcat-8.0.33.tar.gz -C /opt/tomcat --strip-components=1
+
+#setup proper permission
+cd /opt/tomcat
+sudo chgrp -R tomcat conf
+sudo chmod g+rwx conf
+sudo chmod g+r conf/*
+sudo chown -R tomcat logs/ temp/ webapps/ work/
+
+sudo chgrp -R tomcat bin
+sudo chgrp -R tomcat lib
+sudo chmod g+rwx bin
+sudo chmod g+r bin/*
+
+sudo systemctl start tomcat.service
+sudo systemctl enable tomcat.service
